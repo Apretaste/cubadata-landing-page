d7dfbff5feccdf53131c065b5b37db5f9f6c805a
vilferalvarez <vilferalvarez>
2018-07-15 18:54:10 -0400

    Mejoras

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,10 +120,96 @@
 		$('.scrolly').scrolly({
 			speed: 1000
 		});
-$("#show").click(function(){
+			var $header = $('#header');
+
+		if ($header.length > 0) {
+
+			// Shrink effect.
+				/*$main
+					.scrollex({
+						mode: 'top',
+						enter: function() {
+							$nav.addClass('alt');
+						},
+						leave: function() {
+							$nav.removeClass('alt');
+						},
+					});*/
+
+			// Links.
+				var header_a = $header.find('a');
+
+				header_a
+					.scrolly({
+						speed: 1000,
+						offset: function() { return $nav.height(); }
+					})
+					.on('click', function() {
+
+						var $this = $(this);
+
+						// External link? Bail.
+							if ($this.attr('href').charAt(0) != '#')
+								return;
+
+						// Deactivate all links.
+							header_a
+								.removeClass('active')
+								.removeClass('active-locked');
+
+						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+							$this
+								.addClass('active')
+								.addClass('active-locked');
+
+					})
+					.each(function() {
+
+						var	$this = $(this),
+							id = $this.attr('href'),
+							$section = $(id);
+
+						// No section for this link? Bail.
+							if ($section.length < 1)
+								return;
+
+						// Scrollex.
+							$section.scrollex({
+								mode: 'middle',
+								initialize: function() {
+
+									// Deactivate section.
+										if (browser.canUse('transition'))
+											$section.addClass('inactive');
+
+								},
+								enter: function() {
+
+									// Activate section.
+										$section.removeClass('inactive');
+
+									// No locked links? Deactivate all links and activate this section's one.
+										if (header_a.filter('.active-locked').length == 0) {
+
+											header_a.removeClass('active');
+											$this.addClass('active');
+
+										}
+
+									// Otherwise, if this section's link is the one that's locked, unlock it.
+										else if ($this.hasClass('active-locked'))
+											$this.removeClass('active-locked');
+
+								}
+							});
+
+					});
+
+		}
+/*$("#show").click(function(){
 	$("#text").fadeToggle(3000);
 	 $("#graph").fadeToggle(3000);
-});
+});*/
 
 
 })(jQuery);
